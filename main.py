630a35485248170d317527ad810ea96ebd21d620
rinshifu <jasonappleton502@gmail.com>
2025-02-22 06:22:36 -0800

    Added player movement and asteroid

--- main.py
+++ main.py
@@ -4,25 +4,40 @@
 import pygame
 from constants import *
 from player import *
-
+from asteroid import *
+from asteroidfield import *
 
 
 def main():
     running = True
-    black = (0, 0, 0)
     dt = 0
     framerate = 60
     pygame.init
     screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     time = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids,updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(black)
-        player.draw(screen)
-        player.update(dt)
+        
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = time.tick(framerate) / 1000
